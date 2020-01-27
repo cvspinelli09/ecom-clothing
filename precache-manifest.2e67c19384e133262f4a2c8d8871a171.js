2b717c4fba667fbcdc5d3a06472ecfeebb400327
cvspinelli <cvspinelli09@gmail.com>
2020-01-27 13:48:51 +0000

    Updates

---
+++ precache-manifest.2e67c19384e133262f4a2c8d8871a171.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6f346a446d13a7f6a8af9b625ee1d8",
+    "url": "/ecom-clothing/index.html"
+  },
+  {
+    "revision": "57b1fec4ea4360292b1d",
+    "url": "/ecom-clothing/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2d65f6d7c0771a88452c",
+    "url": "/ecom-clothing/static/js/2.63b8f168.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/ecom-clothing/static/js/2.63b8f168.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2b1e869819538e2c69a",
+    "url": "/ecom-clothing/static/js/3.4e91fbc1.chunk.js"
+  },
+  {
+    "revision": "667daff6c86a3c21ff9f",
+    "url": "/ecom-clothing/static/js/4.e711a9ca.chunk.js"
+  },
+  {
+    "revision": "fb632c94eeebd689eaf6",
+    "url": "/ecom-clothing/static/js/5.c17f2473.chunk.js"
+  },
+  {
+    "revision": "2b800eed719dc3b930bd",
+    "url": "/ecom-clothing/static/js/6.33a2ab6c.chunk.js"
+  },
+  {
+    "revision": "2480ed48aad62f5e8437",
+    "url": "/ecom-clothing/static/js/7.4d8fe672.chunk.js"
+  },
+  {
+    "revision": "fd9bd06fc6cde6687b14",
+    "url": "/ecom-clothing/static/js/8.de4cd3fc.chunk.js"
+  },
+  {
+    "revision": "47a84a28dd7609b3a42d",
+    "url": "/ecom-clothing/static/js/9.03dd6bd7.chunk.js"
+  },
+  {
+    "revision": "57b1fec4ea4360292b1d",
+    "url": "/ecom-clothing/static/js/main.17256654.chunk.js"
+  },
+  {
+    "revision": "cd367fc6346694905475",
+    "url": "/ecom-clothing/static/js/runtime-main.b8bce89a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecom-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ecom-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
